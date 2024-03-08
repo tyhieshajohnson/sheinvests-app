9177afe94d3b8d694adc3f5ed782d65ec98720eb
tyhieshajohnson <johnsontyhiesha@gmail.com>
2024-03-08 09:15:15 +0200

    ENV Repair

--- backend/config/index.js
+++ backend/config/index.js
@@ -4,10 +4,10 @@ import { config } from "dotenv";
 config();
 
 let pool = mysql.createPool({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD
+    HOST: process.env.HOST,
+    DATABASE: process.env.DATABASE,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD
 }).promise();
 
 export{
